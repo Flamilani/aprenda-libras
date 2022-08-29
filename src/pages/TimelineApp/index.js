7dbef01c43e3411c4539d8ba5adabcebb25ccefa
Flavio Milani <milanidesign7@gmail.com>
2022-08-29 12:31:44 -0300

    Corrigido para pagina index em netlify

--- src/pages/TimelineApp/index.js
+++ src/pages/TimelineApp/index.js
@@ -2,6 +2,20 @@ import { Breadcrumb } from '../../components/Breadcrumb';
 import TimelineProps from './VerticalTimeline';
 import 'react-vertical-timeline-component/style.min.css';
 import './style.css';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#yourAppElement');
+
+/* const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  }; */
 
 function TimelineApp() {
     return (
