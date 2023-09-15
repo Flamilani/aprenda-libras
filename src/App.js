1f28e8e3a4f708e55d01c63fd077a4c8f97a2884
Flavio Milani <milanidesign7@gmail.com>
2023-09-15 17:00:25 -0300

    Adicionado vLibras

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import RouteApp from './routes/routes';
 import ShowHeader from './shared/locations/ShowHeader';
 import ShowFooter from './shared/locations/ShowFooter';
+import VLibras from '@djpfs/react-vlibras';
 
 import './global/styles.css';
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <Fragment>
       <main>
+        <VLibras />
          <ShowHeader />
         <article>
           <RouteApp />
